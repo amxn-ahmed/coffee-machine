53cb2660a6cb3b0d8287b550cf20581075c5f786
Aman Ahmed <98462412+amxn-ahmed@users.noreply.github.com>
2025-05-27 13:08:33 +0300

    Initial upload of coffee machine Python project

---
+++ main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+machine_off = False
+
+while not machine_off:
+    choice = input(f"what would u like? ({menu.get_items()}) ")
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        machine_off = True
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
+
